9cf8d4784c203ff34c53e0db6a2b4862309604c6
Vesnud <Vesnud@yandex.ru>
2022-12-05 04:12:05 +0300

    2 commit

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('1')
+import pygame
+
+pygame.display.set_caption('Что-то')
+runGame = True
+
+
+def game():
+    global runGame
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+
+    while runGame:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runGame = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        pass
+    pygame.quit()
+
+
+try:
+    w, n = list(map(int, input().split()))
+    size = w * n * 2, w * n * 2
+    if type(w).__name__ != 'int' or type(n).__name__ != 'int':
+        raise ValueError
+    game()
+except ValueError:
+    print('Неправильный формат ввода')
+
